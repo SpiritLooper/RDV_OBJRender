7917cafc6b3c9281123d49ab944d30792071bbfc
SpiritLooper <julienclaisse@orange.fr>
2020-01-30 17:06:44 +0100

    Dessin d'une ligne

--- obj_render.cpp
+++ obj_render.cpp
@@ -5,20 +5,71 @@
 #include <vector>
 #include "geometry.h"
 
-void render() {
+struct Line {
+    int x1, y1, x2, y2;
+    Vec3f color;
+    
+    void draw( std::vector<Vec3f> &framebuffer, const int width, const int height ) {
+        int dx , dy , x , y;
+        int derror, error;
+
+        bool steep = false;
+        if( std::abs(x1 - x2) < std::abs(y1 - y2)) {
+            std::swap(x1, y1);
+            std::swap(x2, y2);
+            steep = true;
+        }
+
+        if( x1 > x2 ) {
+            std::swap(x1, x2);
+            std::swap(y1, y2);
+        }
+
+        dx = x2 - x1;
+        dy = y2 - y1;
+        y = y1;
+        error = 0;
+        derror = std::abs(dy) * 2;
+        
+        for ( x = x1; x  <= x2 ; x++)
+        {
+            if( 0 <= x && x < width && 0 <= y && y < height ) {
+                if(steep) {
+                    framebuffer[ y + x * width] = color;
+                } else {
+                    framebuffer[ x + y * width] = color;
+                }
+            }
+            error += derror;
+            if ( error > dx) {
+                y += ( y2 > y1 ? 1 : -1); 
+                error -= dx * 2;
+            }
+        }
+        
+    }
+};
+
+
+void render(Line &l) {
     const int width = 1024;
     const int height = 768;
 
     std::vector<Vec3f> framebuffer(width * height);
 
+    // Dessin du fond
     for (size_t j = 0; j < height; j++)
     {
         for (size_t i = 0; i < width; i++)
         {
-            framebuffer[i + j * width] = Vec3f( 0 , float(i) /float(width) ,  float(j)  / float(height) );
+            framebuffer[i + j * width] = Vec3f( 0.2 , 0.2, 0.2 );
         }
     }
+
+    // Dessin de la ligne
+    l.draw(framebuffer,width, height);
     
+    // Rendu en image
     std::ofstream ofs;
     ofs.open("./out.ppm");
     ofs << "P6" << std::endl << width << " " << height << std::endl << "255" << std::endl;
@@ -34,6 +85,7 @@ void render() {
 
 int main(int argc, char const *argv[])
 {
-    render();
+    Line line = { 500 , 458 , 25, 25 ,  Vec3f(.12, 1 , .45) };
+    render(line);
     return 0;
 }
